cf19013c2b755d8cde86db327ad376b2990cd526
Filipe Zaidan <felipezaidan10@gmail.com>
2023-08-24 10:42:25 -0300

    refactor: code otimization

--- main.py
+++ main.py
@@ -1,94 +1,42 @@
 from pilha import PilhaArray
-from gera_lab import gera_lab, print_lab 
-labirinto = gera_lab(7,15)
+from gera_lab import gera_lab, print_lab
+from labirinto import Labirinto
 
-pilha = PilhaArray()
-inicio = (1,0)
-posicaoAtual = inicio
-fim =  (len(labirinto)-2, len(labirinto[0])-1)
+def eh_possivel_sair(labirinto):
+    steps = PilhaArray()
+    inicio = (1,0)
+    fim =  (len(labirinto)-2, len(labirinto[0])-1)
 
+    lab = Labirinto(labirinto,inicio,fim)
+    lab.start()
 
-def toBack():
-    global posicaoAtual
-    posicaoAtual = pilha.pop()
+    while lab.position != fim:
+        line, col = lab.position
 
-def toRight():
-    global posicaoAtual
-    line, column = posicaoAtual
-    newPosition = (line,column + 1)
+        if lab.isAvailable((line,col+1)):
+            steps.push(lab.position)
+            lab.toRight()
+        elif lab.isAvailable((line+1, col)):
+            steps.push(lab.position)
+            lab.toDown()
+        elif lab.isAvailable((line-1, col)):
+            steps.push(lab.position)
+            lab.toUp()
+        elif lab.isAvailable((line,col-1)):
+            steps.push(lab.position)
+            lab.toLeft()
+        else:
+            if(lab.position != inicio):
+                prevPosition = steps.pop()
+                lab.goBack(prevPosition)
+            else:
+                break
 
-    # if column + 1 <= len(labirinto[0]):
-    labirinto[line][column+1] = "*"
-    pilha.push(posicaoAtual)
-    posicaoAtual = newPosition
-
-
-def toLeft():
-    global posicaoAtual
-    line, column = posicaoAtual
-    newPosition = (line,column - 1)
-
-    # if column > 0:
-    labirinto[line][column-1] = "*"
-    pilha.push(posicaoAtual)
-    posicaoAtual = newPosition
-
-def toUp():
-    global posicaoAtual
-    line, column = posicaoAtual
-    newPosition = (line-1,column)
-
-    # if line > 0:
-    labirinto[line-1][column] = "*"
-    pilha.push(posicaoAtual)
-    posicaoAtual = newPosition
-
-def toDown():
-    global posicaoAtual
-    line, column = posicaoAtual
-    newPosition = (line+1,column)
-
-    # if line + 1 <= len(labirinto):
-    labirinto[line+1][column] = "*"
-    pilha.push(posicaoAtual)
-    posicaoAtual = newPosition
-
-def start(inicio):
-    line, column = inicio
-    labirinto[line][column] = "*"
-
-start(inicio)
-
-
-def isAvailable(position):
-    line, column = position
-
-    if labirinto[line][column] != "#" and labirinto[line][column] != "*":
-       return True
+    print_lab(lab._labirinto)
+    if lab.position == fim:
+        return True
     else:
         return False
 
-
-while posicaoAtual != fim:
-    line, column = posicaoAtual
-
-    
-    if isAvailable((line,column+1)):
-        toRight()
-    elif isAvailable((line+1, column)):
-        toDown()
-    elif isAvailable((line-1, column)):
-        toUp()
-    elif isAvailable((line,column-1)):
-        toLeft()
-    else:
-        if(posicaoAtual != inicio):
-            toBack()
-        else:
-            break
-        
-    
-
-print(pilha)
-print("posição atual: ", posicaoAtual)
-print_lab(labirinto) #imprime o lab
+labirinto = gera_lab(7,15)
+print(eh_possivel_sair(labirinto))
